b87d051f396d7652a5d598e9c95981ad614b3f26
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 10:59:44 -0300

    Saving files before refreshing line endings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+
+        <div className="background-image flex items-center justify-center content-center">
+          <div className="bg-default-blue w-60 h-10 rounded">TESTE</div>
+        </div>
+    </>
+  );
+}
+
+export default App;
